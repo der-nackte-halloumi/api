c5d6620f61bab8d7b1e78c0814ae9960bdd244b7
Christian Ost <christian@christianost.de>
2020-01-19 23:36:11 +0100

    feat: only push needed images

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -6,19 +6,17 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 function image_with_tag_exists () {
   if test ! -z "$(docker images -q $1)"; then
+    echo "image dernacktehalloumi/api:$1 does exist"
     return 0
   fi
+  
+  echo "image dernacktehalloumi/api:$1 does not exist"
   return 1
 }
 
-image_with_tag_exists "dernacktehalloumi/api:latest"
+image_with_tag_exists "dernacktehalloumi/api:$1"
 if [ "$?" -eq 0 ]; then
-  echo "--> pushing dernacktehalloumi/api:latest"
+  echo "--> pushing dernacktehalloumi/api:$1"
   docker push dernacktehalloumi/api:latest
 fi
 
-image_with_tag_exists "dernacktehalloumi/api:$TRAVIS_BUILD_NUMBER"
-if [ "$?" -eq 0 ]; then
-  echo "--> pushing dernacktehalloumi/api:$TRAVIS_BUILD_NUMBER"
-  docker push dernacktehalloumi/api:$TRAVIS_BUILD_NUMBER
-fi
