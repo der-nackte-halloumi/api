638ea0a3c80b12043d7376bae558487620a614ad
Christian Ost <christian@christianost.de>
2020-01-19 23:41:52 +0100

    fix: pass tags properly around for pushing to docker

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -4,19 +4,19 @@ set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-function image_with_tag_exists () {
+function image_exists () {
   if test ! -z "$(docker images -q $1)"; then
-    echo "image dernacktehalloumi/api:$1 does exist"
+    echo "image $1 does exist"
     return 0
   fi
   
-  echo "image dernacktehalloumi/api:$1 does not exist"
+  echo "image $1 does not exist"
   return 1
 }
 
-image_with_tag_exists "dernacktehalloumi/api:$1"
+image_exists "dernacktehalloumi/api:$1"
 if [ "$?" -eq 0 ]; then
   echo "--> pushing dernacktehalloumi/api:$1"
-  docker push dernacktehalloumi/api:latest
+  docker push dernacktehalloumi/api:$1
 fi
 
