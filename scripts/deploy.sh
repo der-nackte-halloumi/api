72d50ce0012de8514b034c38fc45b7adb356c02a
Christian Ost <christian@christianost.de>
2020-01-23 23:13:33 +0100

    feat: restart with specific image

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,13 @@
 
 set -eo pipefail
 
-./scripts/docker_push.sh $1
+echo ">>> pushing latest image"
+./scripts/docker_push.sh latest
 
 chmod 400 der-nackte-halloumi-api-travis
 
 echo $SERVER_PUBLIC_KEY >> $HOME/.ssh/known_hosts
 
 # restart service
-ssh -i der-nackte-halloumi-api-travis $SERVER_ADDRESS "docker-compose pull api && docker-compose up --build -d api"
+echo ">>> restarting api with image dernacktehalloumi/api:$TRAVIS_BUILD_NUMBER"
+ssh -i der-nackte-halloumi-api-travis $SERVER_ADDRESS "API_IMAGE_TAG=$TRAVIS_BUILD_NUMBER docker-compose up --build -d api"
