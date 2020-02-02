d68f707a639489413529caabafd21e12f27ac47f
Christian Ost <christian@christianost.de>
2020-02-02 22:35:56 +0100

    chore: create seed file and initial shops

---
+++ seeds/initial.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+  shops (id, name, address, lat, long)
+VALUES
+  ('7c3153b9-edb0-4ae4-a068-8c9d06532ea5', 'Original Unverpackt', 'Wiener Straße 16, 10999 Berlin', 52.4978374, 13.428037),
+  ('d7106377-c6e3-4d4e-bf49-b077e719fbe7', 'YES FUTURE Positive Supermarket', 'Carrer de Viladomat, 66, 08015 Barcelona, Spain', 41.3781588, 2.1572628);
