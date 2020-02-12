2edec1e62b547f17011697322c235f9327a38411
Christian Ost <christian@christianost.de>
2020-02-12 16:23:28 +0100

    refactor: remove nesting

--- interface/rest/shops_handler.go
+++ interface/rest/shops_handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/der-nackte-halloumi/api/domain"
 	rest "github.com/der-nackte-halloumi/api/interface/rest/models"
 	"github.com/der-nackte-halloumi/api/usecase/search_shop"
 )
@@ -26,21 +27,14 @@ func (s *shopsHandler) Search(w http.ResponseWriter, req *http.Request) {
 	lat, _ := strconv.ParseFloat(query.Get("lat"), 32)
 	long, _ := strconv.ParseFloat(query.Get("long"), 32)
 
+	var result []domain.Shop
+	var err error
 	if len(search) == 0 {
-		result, err := s.searchShopService.GetShops(req.Context())
-
-		if err != nil {
-			log.Printf("error when retrieving all shops: %v", err)
-			respond(w, http.StatusInternalServerError, err)
-			return
-		}
-
-		respond(w, http.StatusOK, result)
-		return
+		result, err = s.searchShopService.GetShops(req.Context())
+	} else {
+		// TODO: log/track searches and len(result)
+		result, err = s.searchShopService.FindShopsByQuery(req.Context(), search, float32(lat), float32(long))
 	}
-
-	result, err := s.searchShopService.FindShopsByQuery(req.Context(), search, float32(lat), float32(long))
-	// TODO: log/track searches and len(result)
 
 	if err != nil {
 		log.Printf("error when searching shops: %v", err)
